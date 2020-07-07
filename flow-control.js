1103ce5edcea396e57303cabe9da6ab76fa2a9d4
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-07-07 23:51:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,3 +28,14 @@ function ageChecker(age) {
 function ternaryTeenager(age) {
   return (age >= 13 && age <= 19 ? `You are a teenager` :  `You are not a teenager`);
 }
+
+
+function switchAge(age) {
+  switch(age) {
+    case >= 13 && <= 19 :
+      return `You are a teenager`;
+      break;
+    default:
+      return `You have an age`;
+  }
+}
